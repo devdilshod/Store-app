5319ec0362ff94891ab23e86b0cb3835dea0244e
devdilshod <dilshodtursunboev91@gmail.com>
2026-04-15 19:10:38 +0300

    react-dom added, pages,created, route configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,68 @@
-function App() {
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct
+} from "./pages"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-  return (
-  <h1 className="text-4xl text-center mt-16">
-    Store app
-  </h1>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    children: [
+      {
+        path: '',
+        element: <Landing />
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'cart',
+        element: <Cart />
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />
+      },
+      {
+        path: 'orders',
+        element: <Orders />
+      },
+      {
+        path: 'products',
+        element: <Products />
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />
+      }
+    ],
+    errorElement: <Error />
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />
+  }
+])
+
+function App() {
+  return (<RouterProvider router={router} />
   )
 }
 
